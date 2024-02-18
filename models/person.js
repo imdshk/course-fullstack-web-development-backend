e84a03e74aed2168a3978eefb50e12a281b4f430
imdshk <83509028+imdshk@users.noreply.github.com>
2024-02-18 17:34:01 +0200

    exercise 3.20 completed

--- models/person.js
+++ models/person.js
@@ -14,8 +14,21 @@ mongoose
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+       type: String,
+       minLength: 3,
+       required: true
+    }, 
+    number: {
+        type: String,
+        validate: {
+            validator: function(v) {
+                return /\d{2,3}-\d{5,}/.test(v)
+            },
+            message: props => `${props.value} is not a valid number. Please check if the number is in xxx-xxxxxx or xx-xxxxxx format`
+        },
+        required: true
+    },
 })
 
 personSchema.set("toJSON", {
